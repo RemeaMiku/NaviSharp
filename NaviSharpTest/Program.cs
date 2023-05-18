eb323f22b24ee60e945531ba61e59d17a4586277
RemeaMiku <2020302142257@whu.edu.cn>
2023-05-19 00:35:41 +0800

    Revert "增加欧拉角转换至旋转矩阵；增加元组解构器支持；增加角度范围映射"
    This reverts commit 7f267943d9024e3d4c1ce885c5cc651c3639d994.

--- NaviSharpTest/Program.cs
+++ NaviSharpTest/Program.cs
@@ -1,13 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using NaviSharp;
 Console.WriteLine("NaviSharp");
-var test = new[] { 0, 1, 2, 3, 4, 5 };
-var test1 = test[1..^2];
-var mat = new Matrix<double>(new[,]
-{
-    {1d,2,3 },
-    {4,5,6 },
-    {7,8,9 },
-});
-var range = 1..3;
-Console.WriteLine(test1.ToString());
