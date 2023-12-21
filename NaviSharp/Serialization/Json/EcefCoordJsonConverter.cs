e4309a9017a001e04ab530e2a9a6ea28a38867bd
RemeaMiku <2020302142257@whu.edu.cn>
2023-12-21 14:50:42 +0800

    修复序列化转换器

--- NaviSharp/Serialization/Json/EcefCoordJsonConverter.cs
+++ NaviSharp/Serialization/Json/EcefCoordJsonConverter.cs
@@ -16,17 +16,35 @@ public class EcefCoordJsonConverter : JsonConverter<EcefCoord>
 {
     public override EcefCoord Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        var x = reader.GetDouble();
-        var y = reader.GetDouble();
-        var z = reader.GetDouble();
+        if (reader.TokenType != JsonTokenType.StartObject)
+            throw new JsonException();
+        var dictionary = new Dictionary<string, double>();
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndObject)
+                break;
+            if (reader.TokenType != JsonTokenType.PropertyName)
+                throw new JsonException();
+            var propertyName = reader.GetString();
+            reader.Read();
+            if (reader.TokenType != JsonTokenType.Number)
+                throw new JsonException();
+            dictionary.Add(propertyName!, reader.GetDouble());
+        }
+        var xPropertyName = options.PropertyNamingPolicy?.ConvertName(nameof(EcefCoord.X)) ?? nameof(EcefCoord.X);
+        var yPropertyName = options.PropertyNamingPolicy?.ConvertName(nameof(EcefCoord.Y)) ?? nameof(EcefCoord.Y);
+        var zPropertyName = options.PropertyNamingPolicy?.ConvertName(nameof(EcefCoord.Z)) ?? nameof(EcefCoord.Z);
+        var x = dictionary[xPropertyName];
+        var y = dictionary[yPropertyName];
+        var z = dictionary[zPropertyName];
         return new(x, y, z);
     }
 
     public override void Write(Utf8JsonWriter writer, EcefCoord value, JsonSerializerOptions options)
     {
-        var xPropertyName = options.PropertyNamingPolicy?.ConvertName(nameof(value.X)) ?? nameof(value.X);
-        var yPropertyName = options.PropertyNamingPolicy?.ConvertName(nameof(value.Y)) ?? nameof(value.Y);
-        var zPropertyName = options.PropertyNamingPolicy?.ConvertName(nameof(value.Z)) ?? nameof(value.Z);
+        var xPropertyName = options.PropertyNamingPolicy?.ConvertName(nameof(EcefCoord.X)) ?? nameof(EcefCoord.X);
+        var yPropertyName = options.PropertyNamingPolicy?.ConvertName(nameof(EcefCoord.Y)) ?? nameof(EcefCoord.Y);
+        var zPropertyName = options.PropertyNamingPolicy?.ConvertName(nameof(EcefCoord.Z)) ?? nameof(EcefCoord.Z);
         writer.WriteStartObject();
         writer.WriteNumber(xPropertyName, value.X);
         writer.WriteNumber(yPropertyName, value.Y);
